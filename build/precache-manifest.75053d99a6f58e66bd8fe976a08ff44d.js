f5c3e6ae9acf3ac6b93ea4e02e84afde1d4acbb2
Emmanuel Buckley <ebuckley2389@gmail.com>
2019-05-18 16:13:57 -0500

    added build folder for heroku deployment

---
+++ build/precache-manifest.75053d99a6f58e66bd8fe976a08ff44d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90814517966e70bc180be2456b34e30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b34e6a80253dc642d9d3",
+    "url": "/static/css/main.25daff5d.chunk.css"
+  },
+  {
+    "revision": "f600a08e5fa3f0c6ba3a",
+    "url": "/static/js/2.3e7c2604.chunk.js"
+  },
+  {
+    "revision": "b34e6a80253dc642d9d3",
+    "url": "/static/js/main.ad4c1182.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
